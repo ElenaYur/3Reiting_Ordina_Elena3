d1d78032128833d4c72907d3dd12ce3cb31cdbc3
ElenaYur <elena_oluferova@mail.ru>
2020-11-10 18:57:04 +0300

    Initial commit

---
+++ app/src/main/java/com/example/a3reiting_ordina_elena/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.a3reiting_ordina_elena;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
